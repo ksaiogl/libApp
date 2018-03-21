da90f00c4e6c8b654b926a8b2211d78453aeef42
ksaiogl <sairohith@msupply.com>
2018-03-22 01:10:28 +0530

    i

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', ip: ip, serverHost: os.hostname() });
 });
 
+router.get('/a', function (req, res, next) {
+  var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+  console.log(ip);
+  res.render('index', { title: 'Express', ip: ip, serverHost: os.hostname() });
+});
+
+router.get('/b', function (req, res, next) {
+  var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+  console.log(ip);
+  res.render('index', { title: 'Express', ip: ip, serverHost: os.hostname() });
+});
+
 module.exports = router;
